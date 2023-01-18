979b3fc58546c89139e4b651772e30738be98e98
joe <joeteixeira@laposte.net>
2023-01-18 19:26:38 +0100

    ajout du component de question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { QuestionPanelComponent } from './question-panel/question-panel.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path:'home', component:HomeComponent}
+  {path:'home', component:HomeComponent},
+  {path:'question', component:QuestionPanelComponent},
 ];
 
 @NgModule({
